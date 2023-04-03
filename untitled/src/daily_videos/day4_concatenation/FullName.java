864a161b75ded4ae6d501f14b96b9cdec14320af
HKCigdem <c.emirzaesmer@gmail.com>
2023-04-03 06:19:46 +0200

    FullName

---
+++ untitled/src/daily_videos/day4_concatenation/FullName.java
@@ -0,0 +1,13 @@
+package daily_videos.day4_concatenation;
+
+public class FullName {
+    public static void main(String[] args) {
+
+       String firstName = "Daniel";
+       String  lastName = "Jefferson";
+
+       String fullName = firstName + " " + lastName;
+
+       System.out.println("Your full name is " +  fullName + ".");
+    }
+}
